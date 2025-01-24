adf1fa32eb61cda50f542b6b332ed3801c6db602
woodonghyeon <silhone__@naver.com>
2025-01-24 10:43:38 +0900

    init

---
+++ app/src/main/java/com/example/jin/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jin;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jin.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // WebView 인스턴스를 가져옵니다.
+        WebView webView = findViewById(R.id.webView);
+
+        // WebView에서 JavaScript를 활성화합니다.
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true); // localStorage 사용을 허용
+
+        // PC의 IP 주소로 로컬 서버를 로드합니다.
+        // 'http://172.17.22.82:3000'로 연결합니다.
+        webView.loadUrl("http://54.180.167.86");
+
+        // WebViewClient를 설정하여 새 페이지가 브라우저에서 열리지 않도록 설정합니다.
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
